75b52edd484cb6748ad7732f6df8e8a6dd32761d
hmalc <hayden.malcomson@me.com>
2017-04-07 21:55:21 -0600

    Initial Commit

---
+++ FreqAlerts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FreqAlerts
+//
+//  Created by Hayden Malcomson on 2017-04-07.
+//  Copyright © 2017 Hayden Malcomson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
